59fea5d3008a9fe818d4e1c3cd4bb2d2b1702497
stan6694 <stan6@hotmail.fr>
2016-12-04 16:05:16 +0100

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,7 +39,7 @@
         <div class="container">
             <div class="content">
                 <div class="title">Laravel 5</div>
-                //bla bla bla
+                bla bla bla numero 2
             </div>
         </div>
     </body>
