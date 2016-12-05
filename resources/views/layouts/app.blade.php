1173c725a163e9480b55339172965256732875b4
Paulo <paulo.wint@gmail.com>
2016-12-05 17:37:07 +0100

    buttons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,6 +49,9 @@
                 <ul class="nav navbar-nav">
                     <li><a href="{{ url('/home') }}">Home</a></li>
                     <li><a href="{{ url('/posts') }}">Les articles</a></li>
+                    <li><a href="{{ route('posts.create') }}">Créer un article</a></li>
+                    <li><a href="{{ url('/events') }}">Les évènements</a></li>
+                    <li><a href="{{ route('events.create') }}">Créer un évènement</a></li>
                 </ul>
 
                 <!-- Right Side Of Navbar -->
