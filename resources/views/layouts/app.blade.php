31ded6d02db8d0fbe8177afbd8514762fd8110ca
Paulo <paulo.wint@gmail.com>
2016-12-05 17:10:32 +0100

    bouton

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,12 +48,14 @@
                 <!-- Left Side Of Navbar -->
                 <ul class="nav navbar-nav">
                     <li><a href="{{ url('/home') }}">Home</a></li>
+                    <li><a href="{{ url('/posts') }}">Les articles</a></li>
                 </ul>
 
                 <!-- Right Side Of Navbar -->
                 <ul class="nav navbar-nav navbar-right">
                     <!-- Authentication Links -->
                     @if (Auth::guest())
+
                         <li><a href="{{ url('/login') }}">Login</a></li>
                         <li><a href="{{ url('/register') }}">Register</a></li>
                     @else
