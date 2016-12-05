54668bfc65210ae7e495b810eb1fe945b89bf43e
Paulo <paulo.wint@gmail.com>
2016-12-05 15:45:26 +0100

    maj

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,15 +16,13 @@ Route::get('/', function () {
 });
 
 Route::get('/posts', function () {
-<<<<<<< HEAD
-    return view('posts/create');
-=======
+
     return view('posts');
 });
 
 Route::get('/events/create', function () {
     return view('events');
->>>>>>> 69d308494e1997a8f62d1f8ecd972b245a652860
+
 });
 
 Route::auth();
