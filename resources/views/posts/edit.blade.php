7509ea64668fc7cc0d1de4471f1451ab47619480
Paulo <paulo.wint@gmail.com>
2016-12-06 19:11:25 +0100

    ajouts

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -7,7 +7,6 @@
                 <div class="panel panel-default">
                     <div class="panel-heading">Modifier un article</div>
                     <div class="panel-body">
-                        {{--afficher le formulaire de creation d'article--}}
 
                         {!! Form::model(
                         $post,
